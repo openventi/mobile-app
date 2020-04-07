1555a15e29acf9747df749124b907133a0ad1f03
Pedro Luna Montalvo <petermoon@hotmail.com>
2020-04-07 10:26:40 -0500

    SplashScreen para Android.

--- App.js
+++ App.js
@@ -1,30 +1,42 @@
 import 'react-native-gesture-handler';
 import React, { Component } from 'react';
-import { View, StyleSheet, StatusBar } from 'react-native';
+import { View, StyleSheet, StatusBar, Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import SplashScreen from 'react-native-splash-screen';
 import PairScreen from './Screens/PairScreen';
 import DeviceScreen from './Screens/DeviceScreen';
 import { SCREENS } from './constants';
 
 const Drawer = createDrawerNavigator();
 
-export default App = () => {
-  return (
-    <View style={styles.container}>
-      {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-      <NavigationContainer>
-        <Drawer.Navigator initialRouteName={SCREENS.sync}>
-          <Drawer.Screen name={SCREENS.sync}>{({ navigation }) => <PairScreen navigation={navigation} ventilator="N99" />}</Drawer.Screen>
-          <Drawer.Screen name={SCREENS.monitor} component={DeviceScreen} />
-        </Drawer.Navigator>
-      </NavigationContainer>
-    </View>
-  );
-};
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
     flex: 1,
   },
 });
+
+export default class App extends Component {
+  componentDidMount() {
+    SplashScreen.hide();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        <NavigationContainer>
+          <Drawer.Navigator initialRouteName={SCREENS.sync}>
+            <Drawer.Screen name={SCREENS.sync}>
+              {({ navigation }) => (
+                <PairScreen navigation={navigation} ventilator="N99" />
+              )}
+            </Drawer.Screen>
+            <Drawer.Screen name={SCREENS.monitor} component={DeviceScreen} />
+          </Drawer.Navigator>
+        </NavigationContainer>
+      </View>
+    );
+  }
+}
