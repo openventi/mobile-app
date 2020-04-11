851f19794c8a45e34d40fa7da8fa072f31c0022d
ayelen.guerra117@gmail.com <ayelen.guerra117@gmail.com>
2020-04-10 21:42:20 -0300

    Se setea la fuente System. Se hacen arreglos de estilo en la barra de navegación inferior, se elimina el margen y se permite que los botones floten sobre la barra del menú.

--- components/BottomNav.js
+++ components/BottomNav.js
@@ -7,65 +7,67 @@ import LogoSVG from '../components/LogoSVG';
 
 const BottomNav = ({ toggleDrawer }) => {
     return (
-    <View>
-        <View style={styles.barraBotones}>
-            
-            <TouchableOpacity style={styles.divSetting} underlayColor="white">
-                <LogoSVG name='settings' width="24" height="18.3" />  
-                <Animated.View style={styles.bgFill} />              
-            </TouchableOpacity>
+        <View>
+            <View style={styles.buttonBar}>
 
-            <TouchableOpacity style={styles.divAlarm} underlayColor="#16AB1F">
-                <LogoSVG name='alarm' width="24" height="18.3"/>
-            </TouchableOpacity>
-           
+                <TouchableOpacity style={styles.divSetting} underlayColor="white">
+                    <LogoSVG name='settings' width="24" height="18.3" />
+                    <Animated.View style={styles.bgFill} />
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.divAlarm} underlayColor="#16AB1F">
+                    <LogoSVG name='alarm' width="24" height="18.3" />
+                </TouchableOpacity>
+
+            </View>
+            <View style={styles.bottomNav}>
+                <TouchableOpacity
+                    style={styles.iconButton}
+                    onPress={() => {
+                        toggleDrawer();
+                    }}>
+                    <FontAwesomeIcon icon={faBars} color="white" />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.iconButton}>
+                    <FontAwesomeIcon icon={faEllipsisV} color="white" />
+                </TouchableOpacity>
+            </View>
         </View>
-        <View style={styles.bottomNav}>
-            <TouchableOpacity
-                style={styles.iconButton}
-                onPress={() => {
-                    toggleDrawer();
-                }}>
-                <FontAwesomeIcon icon={faBars} color="white" />
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.iconButton}>
-                <FontAwesomeIcon icon={faEllipsisV} color="white" />
-            </TouchableOpacity>
-        </View>
-    </View>
     );
 };
 
 export default BottomNav;
 
 const styles = StyleSheet.create({
-	bottomNav: {
+    bottomNav: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        width: '100%',		
-		paddingTop: 30,
-		paddingHorizontal: 10,
-		paddingBottom: 20,
-		
-	},
-	iconButton: {
-		padding: 5,
-		alignItems: 'center',
+        width: '100%',
+        paddingTop: 15,
+        paddingHorizontal: 10,
+        paddingBottom: 15,
+        backgroundColor: '#0F1416',
     },
-    barraBotones: {
-        
+    iconButton: {
+        padding: 5,
+        alignItems: 'center',
+    },
+    buttonBar: {
+        zIndex: 1,
+        position: 'absolute',
         flexDirection: 'row',
         justifyContent: 'center',
+        alignSelf: 'center',
+        top: '-50%'
     },
-    button: {                
+    button: {
         textAlign: 'center'
-
     },
     divSetting: {
         backgroundColor: '#192327',
-        borderTopLeftRadius: 28,        
+        borderTopLeftRadius: 28,
         borderBottomLeftRadius: 28,
-        marginRight: .5,    
+        marginRight: .5,
         width: 64,
         height: 56,
         alignItems: 'center',
@@ -74,12 +76,12 @@ const styles = StyleSheet.create({
 
     divAlarm: {
         backgroundColor: '#192327',
-        borderTopRightRadius: 28,        
+        borderTopRightRadius: 28,
         borderBottomRightRadius: 28,
-        marginLeft: .5,    
+        marginLeft: .5,
         width: 64,
         height: 56,
-        alignItems: 'center',       
+        alignItems: 'center',
         justifyContent: 'center'
     },
     bgFill: {
